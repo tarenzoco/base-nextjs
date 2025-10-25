53db7b39d85f73d1941845c98eed219d47eeafd5
tarenzoco <tarenzo.co@gmail.com>
2025-10-25 15:11:20 +0530

    Create index.js , for telling Vercel that I have a page.

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+      <h1>🚀 TARENZO Base Next.js</h1>
+      <p>Deployed on Vercel | Connected to Supabase</p>
+    </main>
+  )
+}
